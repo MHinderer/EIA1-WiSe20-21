936d4ae477f98c960c6603c700f86257c7327861
Moritz <hinderer98@gmail.com>
2020-11-26 16:30:22 +0100

    Aufgabe05 ausgeklammert für Powershell & ganzen Code für 06 neu eingetippt

--- Steckbrief/Aufgabe06/script/script06.js
+++ Steckbrief/Aufgabe06/script/script06.js
@@ -1,65 +1,63 @@
-//Zahlenwerte der Länder
+//Zahlenwerte
 //Europa
 var europa2008 = 4965.7;
 var europa2018 = 4209.3;
 //Nord Amerika
 var nordAmerika2008 = 6600.4;
 var nordAmerika2018 = 6035.6;
-//suedAmerika
+//Süd Amerika
 var suedAmerika2008 = 1132.6;
 var suedAmerika2018 = 1261.5;
-//afrika
+//Afrika
 var afrika2008 = 1028;
 var afrika2018 = 1235.5;
-//Asia
-var asia2008 = 12954.7;
-var asia2018 = 16274.1;
+//Asien
+var asien2008 = 12954.7;
+var asien2018 = 16274.1;
 //Australien
-var australia2008 = 1993;
-var australia2018 = 2100.5;
-//GesamtEmission
-var gesEmission = afrika2018 + suedAmerika2018 + europa2018 + nordAmerika2018 + asia2018 + australia2018;
-// Ausgabe
+var australien2008 = 1993;
+var australien2018 = 2100.5;
+//gesEmission
+var gesEmission = europa2018 + nordAmerika2018 + suedAmerika2018 + afrika2018 + asien2018 + australien2018;
+//Ausgabe
 window.addEventListener('load', function () {
     document.querySelector(".europe").addEventListener("click", function () {
-        domManipulator("Europa", europa2018, europa2008);
+        domManipulator("Europe", europa2018, europa2008);
     });
 });
 window.addEventListener('load', function () {
     document.querySelector(".northamerica").addEventListener("click", function () {
-        domManipulator("Nord Amerika", nordAmerika2018, nordAmerika2008);
+        domManipulator("North America", nordAmerika2018, nordAmerika2008);
     });
 });
 window.addEventListener('load', function () {
-    document.querySelector(".southamerica").addEventListener("click", function () {
-        domManipulator("Süd Amerika", suedAmerika2018, suedAmerika2008);
+    document.querySelector(".southhamerica").addEventListener("click", function () {
+        domManipulator("South America", suedAmerika2018, suedAmerika2008);
     });
 });
 window.addEventListener('load', function () {
     document.querySelector(".africa").addEventListener("click", function () {
-        domManipulator("Afrika", afrika2018, afrika2008);
+        domManipulator("Africa", afrika2018, afrika2008);
     });
 });
 window.addEventListener('load', function () {
     document.querySelector(".asia").addEventListener("click", function () {
-        domManipulator("Asien", asia2018, asia2008);
+        domManipulator("Asia", asien2018, asien2008);
     });
 });
 window.addEventListener('load', function () {
     document.querySelector(".australia").addEventListener("click", function () {
-        domManipulator("Australia", australia2018, australia2008);
+        domManipulator("Australia", australien2018, australien2008);
     });
 });
 //Funktion
 function domManipulator(kontinent, kontinent2018, kontinent2008) {
-    //Text
     document.querySelector(".titleRegion").innerHTML = kontinent;
     document.querySelector(".titleRegion2").innerHTML = kontinent;
-    document.querySelector("#absoluteEmi").innerHTML = kontinent2018.toFixed(2) + " CO2 kg";
+    document.querySelector("#absoluteEmi").innerHTML = kontinent2018.toFixed(2) + "CO2 kg";
     document.querySelector("#totalEmi").innerHTML = (100 / (gesEmission / kontinent2018)).toFixed(2) + "%";
     document.querySelector("#growthPerEmi").innerHTML = (((kontinent2018 / kontinent2008) - 1) * 100).toFixed(2) + "%";
-    document.querySelector("#growthAbsEmi").innerHTML = (kontinent2018 - kontinent2008).toFixed(2) + " CO2 kg";
-    //Diagramm
-    document.querySelector('.chart').setAttribute('style', 'height:' + (100 / (gesEmission / kontinent2018)).toFixed(2) + "%");
+    document.querySelector("#growthAbsEmi").innerHTML = (kontinent2018 - kontinent2008).toFixed(2) + "CO2 kg";
+    document.querySelector(".chart").setAttribute('style', 'height:' + (100 / (gesEmission / kontinent2018)).toFixed(2) + "%");
 }
 //# sourceMappingURL=script06.js.map
